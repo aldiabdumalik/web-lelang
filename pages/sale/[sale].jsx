971943c0f32a0acfc2519867f55286dfe1781fe6
agungd3v <agungd3v@gmail.com>
2023-03-20 10:45:25 +0700

    update config next

--- pages/sale/[sale].jsx
+++ pages/sale/[sale].jsx
@@ -1,9 +1,21 @@
 import PageLayout from "@/layouts/PageLayout";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Detail() {
   return (
     <PageLayout>
-      <div className="container">asd</div>
+      <div className="container flex flex-col items-center">
+        <div className="w-full md:w-2/3 pt-7 pb-12">
+          <div className="flex items-center">
+            <Link>
+              <Image />
+              <span>Kembali Ke Pencarian</span>
+            </Link>
+          </div>
+        </div>
+        <div className="w-full md:w-2/3">asd</div>
+      </div>
     </PageLayout>
   )
 }
