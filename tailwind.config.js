2a17c551d255a0289a0747c5b632495e70071d43
Abdu <aldiabdumalik98@gmail.com>
2023-03-20 16:50:09 +0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,35 @@ module.exports = {
       colors: {
         "primary": "#00529D",
         "secondary": "#F37022",
-        "disabled": "#DEE3EC"
-      }
+        "disabled": "#DEE3EC",
+        'primary-30': '#6CBAF8',
+        'primary-text': '#171A1C',
+        'secondary-text': '#73808C',
+      },
+      height: {
+        '720px': '45rem',
+        '384px': '384px',
+        '451px': '451px',
+        '552px': '552px',
+        '644px': '644px',
+      },
+      width: {
+        '271': '271px',
+        '364': '364px'
+      },
+      backgroundImage: {
+        'banner': "url('../public/img/banner.png')",
+        'banner-mobile': "url('../public/img/banner-mobile.png')",
+      },
+      backgroundPosition: {
+        'top-4': 'right 55% bottom 100%',
+      },
+      fontSize: {
+        '40': '40px',
+      },
+      padding: {
+        '18': '68px'
+      },
     },
     screens: {
       sm: "480px",
