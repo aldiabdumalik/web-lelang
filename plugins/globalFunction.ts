d0209f743cfb31b454bd6ed8195c18b216f71c20
agungd3v <agungd3v@gmail.com>
2023-03-21 07:55:48 +0700

    add global function format rupiah

--- plugins/globalFunction.ts
+++ plugins/globalFunction.ts
@@ -1,22 +1,15 @@
-interface FormatIDR {
-  angka: string | number;
-  prefix: string
-}
+export const formatIDR = (angka: string | number, prefix: string) => {
+  let number = angka.toString().replace(/[^,\d]/g, '');
+	let split = number.split(',');
+	let rest = split[0].length % 3;
+	let rupiah = split[0].substr(0, rest);
+	let thousand = split[0].substr(rest).match(/\d{3}/gi);
 
-export const formatIDR = ({angka, prefix}: FormatIDR) => {
-  console.log(angka, prefix);
-  return angka;
-  // let number = angka.replace(/[^,\d]/g, '').toString();
-	// let split = number.split(',');
-	// let sisa = split[0].length % 3;
-	// let rupiah = split[0].substr(0, sisa);
-	// let ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+	if (thousand) {
+		let separator = rest ? '.' : '';
+		rupiah += separator + thousand.join('.');
+	}
 
-	// if (ribuan) {
-	// 	let separator = sisa ? '.' : '';
-	// 	rupiah += separator + ribuan.join('.');
-	// }
-
-	// rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-	// return prefix == undefined ? rupiah : (rupiah ? 'Rp' + rupiah : '');
+	rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+	return prefix == undefined ? rupiah : (rupiah ? 'Rp' + rupiah : '');
 }
