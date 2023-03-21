9b2078415382608544bb977dff352af6b090237d
agungd3v <agungd3v@gmail.com>
2023-03-21 21:46:22 +0700

    update

--- components/SaleDetailPage/Purchase.jsx
+++ components/SaleDetailPage/Purchase.jsx
@@ -1,12 +1,8 @@
-import { useRouter } from "next/router";
-
 export default function Purchase() {
-  const rts = useRouter();
-
   return (
     <button
       className="w-full bg-primary py-2.5 text-white font-semibold rounded-md"
-      onClick={() => rts.push(`/sale/${rts.query.sale}/calculator`)}
+      onClick={() => alert('oke')}
     >
       Ajukan Pembelian
     </button>
